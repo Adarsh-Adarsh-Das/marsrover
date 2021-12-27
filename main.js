f05efec816c8900f3b57e6106703296f9048b788
Adarsh-Adarsh-Das <90918365+Adarsh-Adarsh-Das@users.noreply.github.com>
2021-12-27 19:58:15 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 canvas=document.getElementById('myCanvas');
 ctx=canvas.getContext("2d");
 
-nasa_mars_images_array = ["nasa_image_1.jpg","nasa_image_2.jpeg", "nasa_image_3.jpg","nasa_image_4.jpg"];
+nasa_mars_images_array = ["nasa_image_1.jpeg","nasa_image_2.jpeg", "nasa_image_3.jpeg","nasa_image_4.jpeg"];
 random_number = Math.floor(Math.random() * 4);
  console.log(random_number);
  
@@ -14,8 +14,6 @@ background_image = nasa_mars_images_array[random_number];
 rover_x=10;
 rover_y=10;
 
-background_image="mars.jpg";
-rover_image="rover.png";
 function add() {
     background_imgTag = new Image();
      background_imgTag.onload = uploadBackground;
